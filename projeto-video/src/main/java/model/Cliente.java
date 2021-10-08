74f09e6202de999bd90e4d2aa184b1f53da04dde
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2021-10-08 11:50:46 -0300

    mudança

--- projeto-video/src/main/java/model/Cliente.java
+++ projeto-video/src/main/java/model/Cliente.java
@@ -5,7 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+// modifiquei
 @Entity
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = 1L;
